bcaa055acb5159c7f5f8d9ec3db511831f3a8a6d
S.Kornev <post.kornev@gmail.com>
2021-02-19 22:41:52 +0300

    Initial Commit

---
+++ CodemakersTeam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodemakersTeam
+//
+//  Created by Сергей Корнев on 19.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
