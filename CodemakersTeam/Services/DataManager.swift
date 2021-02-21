0d445b479519a4babab45ab4faa3f200ee2d06ec
Shangin Vyacheslav <slava-poison@yandex.ru>
2021-02-21 23:55:11 +0400

    add image view, labels, some code 2

--- CodemakersTeam/Services/DataManager.swift
+++ CodemakersTeam/Services/DataManager.swift
@@ -7,16 +7,16 @@
 
 class DataManager {
     static let shared = DataManager()
-    let names = ["Имя1", "Имя2"]
-    let surnames = ["Фамилия1", "Фамилия2"]
-    let photos = ["photo1", "photo2"]
-    let cities = ["city1", "city2"]
-    let familyStatus = ["status1", "status2"]
-    let ages = ["age1", "age2"]
-    let hobbies = ["hobby1", "hobby2"]
-    let jobs = ["job1", "job2"]
-    let jobPositions = ["position1", "position2"]
-    let emails = ["mail1", "mail2"]
-    let phones = ["phone1", "phone2"]
+    let names = ["Имя1", "Имя2", "Вячеслав"]
+    let surnames = ["Фамилия1", "Фамилия2", "Шангин"]
+    let photos = ["photo1", "photo2", "photo3"]
+    let cities = ["city1", "city2", "Тольятти"]
+    let familyStatus = ["status1", "status2", "Женат"]
+    let ages = ["age1", "age2", "34"]
+    let hobbies = ["hobby1", "hobby2", "Рыбалка, туризм"]
+    let jobs = ["job1", "job2", "1с разработчик"]
+    let jobPositions = ["position1", "position2", "Начальник отдела АСУ"]
+    let emails = ["mail1", "mail2", "slava-poison@yandex.ru"]
+    let phones = ["phone1", "phone2", "89272134434"]
     private init() {}
 }
