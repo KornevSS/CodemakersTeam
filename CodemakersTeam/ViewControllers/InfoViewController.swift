2c027d9712fcb95817a0d81bc8ee1bb3f56ea961
S.Kornev <post.kornev@gmail.com>
2021-02-22 20:32:23 +0300

    Writing code for Info ViewController

--- CodemakersTeam/ViewControllers/InfoViewController.swift
+++ CodemakersTeam/ViewControllers/InfoViewController.swift
@@ -10,18 +10,28 @@ import UIKit
 class InfoViewController: UIViewController {
 
     //MARK: - IB Outlets
-    @IBOutlet var PlaceLabel: UILabel!
+    @IBOutlet var placeLabel: UILabel!
     @IBOutlet var statusLabel: UILabel!
     @IBOutlet var ageLabel: UILabel!
     @IBOutlet var jobLabel: UILabel!
     @IBOutlet var positionLabel: UILabel!
-    @IBOutlet var malLabel: UILabel!
+    @IBOutlet var mailLabel: UILabel!
     @IBOutlet var phoneLabel: UILabel!
+    
+    // MARK: - Public properties
+    var person: Person!
+
     
     // MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        placeLabel.text = person.city
+        statusLabel.text = person.familyStatus
+        ageLabel.text = person.age
+        jobLabel.text = person.job
+        positionLabel.text = person.jobPosition
+        mailLabel.text = person.email
+        phoneLabel.text = person.phone
     }
     
 
